afcb577393fac4f2f01ceb1ef98a80d4cd8b1f93
Parkseungboo <96049261+Parkseungboo@users.noreply.github.com>
2022-01-17 17:58:22 +0900

    01-16 수정

--- StarBucks Web Page/js/main.js
+++ StarBucks Web Page/js/main.js
@@ -9,3 +9,26 @@ searchInputE1.addEventListener('click', function(){
     searchE1.classList.add('focused');
     searchInputE1.setAttribute('placeholder', '통합검색');
 })
+
+const badgeE1 = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+    console.log('scroll!');
+
+    if(window, scrollY > 500){
+        // badgeE1.style.display ='none'; 배지 화면에서 사라지게 처리
+
+        gsap.to(badgeE1, .6, {
+            opacity: 0,
+            display: 'none'
+        })
+    }
+    else{
+        // badgeE1.style.display = 'block'; 배지 화면에서 보이게 처리
+
+        gsap.to(badgeE1, .6, {
+            opacity: 1,
+            display: 'block'
+    });
+}
+}, 300))
